b0bb9f89073796c6220bbed00b04bea67f6a64ea
eMills07 <elijah.mills2@stu.daviess.kyschools.us>
2025-02-20 04:20:07 +0000

    I DID IT

--- Lab24d.java
+++ Lab24d.java
@@ -14,13 +14,23 @@ public class Lab24d
 	{
 		Scanner file = new Scanner (new File("lab24d.dat"));
 
+		int x = file.nextInt();
+		file.nextLine();
+		for(int i = 0; i < x; i++)
+		{
+		TicTacToe game = new TicTacToe(file.nextLine());
 
+		
+		//Read how many times you should read
 
-
-
-
-
-
+		//TicTacToe only want this: file.nextline()
+		//Make a TicTacToe for each game in the file
+		//Print the game (TicTacToe)
+		System.out.println(game);
+		//Print the winner
+		System.out.println(game.getWinner() + "\n\n");
+		}
+		file.close();
 	}
 }
 
--- TicTacToe.java
+++ TicTacToe.java
@@ -1,4 +1,4 @@
-//© A+ Computer Science  -  www.apluscompsci.com
+//ï¿½ A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
 //Class - 
@@ -9,37 +9,80 @@ import static java.lang.System.*;
 
 public class TicTacToe
 {
-	private char[][] mat;
+	private char[][] mat = new char[3][3];
 
 	public TicTacToe()
 	{
+		int letter = 0;
+		String game = "XXXOOXXOO";
 
-
+	for(int i = 0; i < mat.length; i++)
+		for(int j = 0; j < mat[0].length; j++)
+		{
+			mat[i][j] = game.charAt(letter);
+			letter++;
+		}
 	}
 
 	public TicTacToe(String game)
 	{
 
+		int letter = 0;
 
-
+	for(int i = 0; i < mat.length; i++)
+		for(int j = 0; j < mat[0].length; j++)
+		{
+			mat[i][j] = game.charAt(letter);
+			letter++;
+		}
 
 	}
 
 	public String getWinner()
 	{
+		//String winner = "";
+		if(mat[0][0] == mat[0][1] && mat[0][1] == mat[0][2])
+		{
+			return mat[0][0] + " wins horizontally!";
+		}
+		else if(mat[1][0] == mat[1][1] && mat[2][1] == mat[1][2])
+		{
+			return mat[1][0] + " wins horizontally!";
+		}
+		else if(mat[2][0] == mat[2][1] && mat[2][1] == mat[2][2])
+		{
+			return mat[2][0] + " wins horizontally!";
+		}
+		else if(mat[0][0] == mat[1][0] && mat[1][0] == mat[2][0])
+		{
+			return mat[2][0] + " wins vertically!";
+		}
+		else if(mat[0][1] == mat[1][1] && mat[1][1] == mat[2][1])
+		{
+			return mat[2][1] + " wins vertically!";
+		}
+		else if(mat[0][2] == mat[1][2] && mat[1][2] == mat[2][2])
+		{
+			return mat[2][2] + " wins vertically!";
+		}
+		else if(mat[0][0] == mat[1][1] && mat[1][1] == mat[2][2])
+		{
+			return mat[0][0] + " wins diagnoly!";
+		}
+		else if(mat[0][2] == mat[1][1] && mat[1][1] == mat[2][0])
+		{
+			return mat[2][0] + " wins diagnolly!";
+		}
+		else
+		{
+			return "cat's game - No Winner";
+		}
 
 
 
 
 
 
-
-
-
-
-
-
-		return "";
 	}
 
 	public String toString()
@@ -49,8 +92,15 @@ public class TicTacToe
 
 
 
+		for(int i = 0; i < mat.length; i++)
+		{
+			for(int j = 0; j < mat[0].length; j++)
+			{
+				output += mat[i][j] + " ";
+			}
+			output += "\n";
+		}
 
-
-		return output+"\n\n";
+		return output+"";
 	}
 }
